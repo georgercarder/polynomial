8d7eb505d20527498384c84e2fc352e41eb688b3
George Carder <georgercarder@gmail.com>
2021-11-24 21:14:29 -0800

    tested construction of a large polynomial

--- README.md
+++ README.md
@@ -6,8 +6,6 @@ Copyright (c) 2021 George Carder georgercarder@gmail.com
 
 GENERAL PUBLIC LICENSE Version 3
 
-status: incomplete as of 11/24/21 needs to be tested
-
 usage
 
 ```
--- polynomial_test.go
+++ polynomial_test.go
@@ -1,7 +1,6 @@
 package polynomial 
 
 import (
-//  "fmt"
   "testing"
   
   "github.com/stretchr/testify/assert"
@@ -9,17 +8,18 @@ import (
 
 func TestPolynomial(t *testing.T) {
   tru := true
+
+  // all below double checked against Wolfram Alpha <3
   
   r1 := NewPolynomialWithRootsInt(2, 3)
   expected1 := NewPolynomialInt(6, -5, 1)
   assert.Equal(t, new(Polynomial).IsSame(r1, expected1), tru, "r1 != expected1")
   
-
   r2 := NewPolynomialWithRootsInt(2, 3, 5)
   expected2 := NewPolynomialInt(-30, 31, -10, 1)
   assert.Equal(t, new(Polynomial).IsSame(r2, expected2), tru, "r2 != expected2")
 
-
-  // TODO test r3 := NewPolynomialWithRootsInt(2, 3, 4, 5, 6, 7, 8, 9, 10)
-  //fmt.Println("debug", r0)*/
+  r3 := NewPolynomialWithRootsInt(2, 3, 4, 5, 6, 7, 8, 9, 10)
+  expected3 := NewPolynomialInt(-3628800, 6999840, -5753736, 2655764, -761166, 140889, -16884, 1266, -54, 1)
+  assert.Equal(t, new(Polynomial).IsSame(r3, expected3), tru, "r3 != expected3")
 }
