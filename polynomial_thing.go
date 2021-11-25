7832251f87fb398479d3d94bdcd3eba25a746630
George Carder <georgercarder@gmail.com>
2021-11-24 18:49:53 -0800

    simplify by breaking into functions

--- polynomial_thing.go
+++ polynomial_thing.go
@@ -12,19 +12,7 @@ type polynomialThing interface {
 
 func mul(a, b polynomialThing) (ret *Polynomial) {
   if a.isSoloTerm() && b.isSoloTerm() {
-    coefficientProduct := new(big.Int).Mul(a.coefficients()[0], b.coefficients()[0])
-    combinedDegree := new(big.Int).Add(a.degree(), b.degree())
-    var coefficientArray []*big.Int
-    idx := new(big.Int).SetInt64(0)
-    zero := new(big.Int).SetInt64(0)
-    bigOne := new(big.Int).SetInt64(1)
-    for ; idx.Cmp(combinedDegree) < 0; { // x < y
-      coefficientArray = append(coefficientArray, zero) 
-      idx = new(big.Int).Add(idx, bigOne) // idx++
-    }
-    // replace last with the product
-    coefficientArray[len(coefficientArray)-1] = coefficientProduct
-    ret = NewPolynomialFromArray(coefficientArray)
+    ret = multiplyTerms(a, b)
     return
   } else if !a.isSoloTerm() && !b.isSoloTerm() {
     // this should repeat ... logic of `PolynomialMultiplication`
@@ -33,28 +21,50 @@ func mul(a, b polynomialThing) (ret *Polynomial) {
         NewBinomial(b.(*Polynomial).head(), b.(*Polynomial).tail()))
     return
   } else {  
-    var soloTerm *term
-    var polynomial *Polynomial
-    if a.isSoloTerm() {
-      soloTerm = a.(*term)
-      polynomial = b.(*Polynomial)
-    } else {
-      soloTerm = b.(*term)
-      polynomial = a.(*Polynomial)
-    }
-    var coefficientProducts []*big.Int
-    for _, c := range polynomial.Coefficients {
-      coefficientProduct := new(big.Int).Mul(c, soloTerm.coefficient)
-      coefficientProducts = append(coefficientProducts, coefficientProduct)
-    }
-    var coefficientArray []*big.Int
-    zero := new(big.Int).SetInt64(0)
-    idx := new(big.Int).SetInt64(0)
-    for ; idx.Cmp(soloTerm.degree()) < 0; { // x < y
-      coefficientArray = append(coefficientArray, zero)
-    }
-    coefficientArray = append(coefficientArray, coefficientProducts...)
-    ret = NewPolynomialFromArray(coefficientArray)
+    ret = distributeTermAcrossPolynomial(a, b)
   }
+  return
+}
+
+func multiplyTerms(a, b polynomialThing) (ret *Polynomial) {
+  coefficientProduct := new(big.Int).Mul(a.coefficients()[0], b.coefficients()[0])
+  combinedDegree := new(big.Int).Add(a.degree(), b.degree())
+  var coefficientArray []*big.Int
+  idx := new(big.Int).SetInt64(0)
+  zero := new(big.Int).SetInt64(0)
+  bigOne := new(big.Int).SetInt64(1)
+  for ; idx.Cmp(combinedDegree) < 0; { // x < y
+    coefficientArray = append(coefficientArray, zero) 
+    idx = new(big.Int).Add(idx, bigOne) // idx++
+  }
+  // replace last with the product
+  coefficientArray[len(coefficientArray)-1] = coefficientProduct
+  ret = NewPolynomialFromArray(coefficientArray)
+  return
+}
+
+func distributeTermAcrossPolynomial(a, b polynomialThing) (ret *Polynomial) {
+  var soloTerm *term
+  var polynomial *Polynomial
+  if a.isSoloTerm() {
+    soloTerm = a.(*term)
+    polynomial = b.(*Polynomial)
+  } else {
+    soloTerm = b.(*term)
+    polynomial = a.(*Polynomial)
+  }
+  var coefficientProducts []*big.Int
+  for _, c := range polynomial.Coefficients {
+    coefficientProduct := new(big.Int).Mul(c, soloTerm.coefficient)
+    coefficientProducts = append(coefficientProducts, coefficientProduct)
+  }
+  var coefficientArray []*big.Int
+  zero := new(big.Int).SetInt64(0)
+  idx := new(big.Int).SetInt64(0)
+  for ; idx.Cmp(soloTerm.degree()) < 0; { // x < y
+    coefficientArray = append(coefficientArray, zero)
+  }
+  coefficientArray = append(coefficientArray, coefficientProducts...)
+  ret = NewPolynomialFromArray(coefficientArray)
   return
 }
